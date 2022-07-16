361c1b02e6be2a1bc67a12cdf854fac3d9aabb7a
shiyindebcd <shiyindebcd@gmail.com>
2022-07-16 18:00:08 +0800

    更新b站地址

--- 自启动脚本.bat
+++ 自启动脚本.bat
@@ -3,5 +3,5 @@ if "%1" == "h" goto begin
 mshta vbscript:createobject("wscript.shell").run("""%~0"" h",0)(window.close)&&exit
 :begin
 C:
-cd E:\进程交易者
+cd C:\Users\shiyinde\Desktop\进程交易者
 python main.py
